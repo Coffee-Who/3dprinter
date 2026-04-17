eb364d65bf94e62eff699922dc274b4a99cd2488
Coffee-Who <barry00000000@gmail.com>
2026-04-17 21:36:38 +0800

    Update portal_app.py

--- portal_app.py
+++ portal_app.py
@@ -1,10 +1,7 @@
-ython
+JSONBIN_MASTER_KEY = st.secrets["$2a$10$i8COKSrTKdtvxAwfLVDbte2AoL7Nar3oLKA7ZgQWZrfrLlEC8YluyY"]
+JSONBIN_BIN_ID = st.secrets["69e2350daaba8821970cbb6c"]
+# 確保請求標頭（Headers）包含 Master Key
 headers = {
     'X-Master-Key': JSONBIN_MASTER_KEY,
     'Content-Type': 'application/json'
-}
-# 存取特定 Bin 的網址
-url = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}"
-JSONBIN_MASTER_KEY = "$2a$10$i8COKSrTKdtvxAwfLVDbte2AoL7Nar3oLKA7ZgQWZrfrLlEC8Yluy"
-JSONBIN_BIN_ID     = "69e2350daaba8821970cbb6c"
 

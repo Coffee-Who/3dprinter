7e47099c8cd213ea4e7890eea61bb9f013f603fc
Coffee-Who <barry00000000@gmail.com>
2026-04-17 21:47:26 +0800

    Update portal_app.py

--- portal_app.py
+++ portal_app.py
@@ -1,10 +1,20 @@
+# 請檢查程式碼中是否有這一段
 import requests
+
 # 確保 URL 是正確的
-url = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}/latest"
-JSONBIN_MASTER_KEY = st.secrets["$2a$10$i8COKSrTKdtvxAwfLVDbte2AoL7Nar3oLKA7ZgQWZrfrLlEC8YluyY"]
-JSONBIN_BIN_ID = st.secrets["69e2350daaba8821970cbb6c"]
-# 確保請求標頭（Headers）包含 Master Key
+url = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}/latest" 
+
 headers = {
-    'X-Master-Key': JSONBIN_MASTER_KEY,
-    'Content-Type': 'application/json'
+    'X-Master-Key': JSONBIN_MASTER_KEY
+}
+
+# 執行請求
+response = requests.get(url, headers=headers)
+data = response.json()
+
+# 注意：JSONBin v3 的資料結構是在 'record' 欄位下
+if 'record' in data:
+    final_data = data['record']
+else:
+    final_data = data # 預設回傳
 

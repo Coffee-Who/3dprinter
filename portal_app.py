2893e48c45cba164e905b31457fc8b89ccfe3c6c
Coffee-Who <barry00000000@gmail.com>
2026-04-17 21:28:51 +0800

    Update portal_app.py

--- portal_app.py
+++ portal_app.py
@@ -1,3 +1,10 @@
+ython
+headers = {
+    'X-Master-Key': JSONBIN_MASTER_KEY,
+    'Content-Type': 'application/json'
+}
+# 存取特定 Bin 的網址
+url = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}"
 JSONBIN_MASTER_KEY = "$2a$10$i8COKSrTKdtvxAwfLVDbte2AoL7Nar3oLKA7ZgQWZrfrLlEC8Yluy"
-  JSONBIN_BIN_ID     = "69e2350daaba8821970cbb6c"
+JSONBIN_BIN_ID     = "69e2350daaba8821970cbb6c"
 

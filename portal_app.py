40bc24af42dd3cd81fc58cf9601b3e8a16c5a75e
Coffee-Who <barry00000000@gmail.com>
2026-04-17 17:43:46 +0800

    Update portal_app.py

--- portal_app.py
+++ portal_app.py
@@ -1,2 +1,2 @@
-JSONBIN_MASTER_KEY = "$2a$10$0oZ3tRXXuInYI61c38vJbuBpQwxtuaXxsKjliUwMm39b2kZDtfD/S"  # ← 貼上 Master Key
+JSONBIN_MASTER_KEY = "$2a$10$NmH85djTPLNNMfQu4xJ.0uOLgN09L1GvrTK4dOP2B/rCzqfH6lo9."  # ← 貼上 Master Key
 JSONBIN_BIN_ID     = "69e1fd2c36566621a8c26bd5"               # ← 先留空
